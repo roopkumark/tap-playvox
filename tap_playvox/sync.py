322654de7535cb6e6d937ce86d006ee97097e906
roopkumark <103514765+roopkumark@users.noreply.github.com>
2023-09-06 00:13:47 -0400

    Updates made on feedback received

--- tap_playvox/sync.py
+++ tap_playvox/sync.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
 
-import time
 import singer
-import json
 from singer import metrics, metadata, Transformer
 from singer.bookmarks import set_currently_syncing
 
@@ -14,7 +12,8 @@ LOGGER = singer.get_logger()
 def write_schema(stream):
     schema = stream.schema.to_dict()
     singer.write_schema(stream.tap_stream_id, schema, stream.key_properties)
-            
+
+
 def sync_endpoint(client,
                   catalog,
                   state,
@@ -24,7 +23,7 @@ def sync_endpoint(client,
                   endpoint,
                   key_bag,
                   stream_params={}):
-    
+
     persist = endpoint.get('persist', True)
 
     if persist:
@@ -32,44 +31,44 @@ def sync_endpoint(client,
         schema = stream.schema.to_dict()
         mdata = metadata.to_map(stream.metadata)
         write_schema(stream)
-    
+
     # API Parameters
     # Reference https://support.agyletime.com/hc/en-us/articles/4402740869401-Integration-Guide-Agent-Metrics
     initial_load = True
     next_page_token=''
-  
+
     params={}
-    
+
     #ISO 8601 Datetime format
     iso_format = "%Y-%m-%dT%H:%M:%S.%fZ"
     start_date = singer.get_bookmark(state,
                                      stream_name,
-                                     'endDate')
+                                     key='endDate',
+                                     default=client.start_date)
 
-    if start_date:
-        start_datetime = singer.utils.strptime_to_utc(start_date)
-    else:
-        # If no start_date or bookmark available, default to start_date of the config
-        start_datetime = singer.utils.strptime_to_utc(client.start_date)
-        
-        dt_string = str(start_datetime)
-        dt_object = datetime.fromisoformat(dt_string)
-        start_datetime = dt_object.strftime(iso_format)
-    
+    start_datetime = singer.utils.strptime_to_utc(start_date)
+
+    # Subtract 14 days from the bookmark datetime for Schedule Metrics since there can be updates
+    # during this period
+    if stream_name == 'schedule_metrics':
+        start_datetime -= timedelta(days=14)
+
+    iso_start_datetime = start_datetime.strftime(iso_format)
+
     if stream_name == 'tasks':
         params = {
-            'taskStartTimeFrom': str(start_datetime),
+            'taskStartTimeFrom': iso_start_datetime,
             'taskStartTimeTo': str(datetime.utcnow().isoformat())
         }
- 
+
     if stream_name == 'agent_metrics' or stream_name == 'schedule_metrics':
         params = {
-            'startTime': str(start_datetime),
+            'startTime': iso_start_datetime,
             'userGrouping': 'true',
             'dateGrouping': 'true'
         }
     path = endpoint['path'].format(**key_bag)
-    
+
     #next_page_token param value for Playvox
     while initial_load or len(next_page_token) > 0:
 
@@ -84,80 +83,80 @@ def sync_endpoint(client,
                               params=params,
                               endpoint=stream_name,
                              )
-        
+
         if data is None:
             return
-        
+
         if 'data_key' in endpoint:
             records = data[endpoint['data_key']]
         else:
-            records = [data]      
-        
-        #if no records are received
+            records = [data]
+
         if not records:
             return
-    
-        #parse records
-        parse=True
-    
+
+        parse = True
         date_records = []
         user_channel_records = []
         parsed_date_records = 0
         parsed_user_records = 0
-        
+
         # For Worstream Agent and Schedule Metrics, only parse the data if Users data is presents
         if (stream_name == 'agent_metrics' or stream_name == 'schedule_metrics')and endpoint['metric_key'] in data[endpoint['data_key']]:
-            date_records = data[endpoint['data_key']][endpoint['metric_key']]  
-        
+            date_records = data[endpoint['data_key']][endpoint['metric_key']]
+
         # For Worstream Agent and Schedule Metrics, if no date records are present return
         if (stream_name == 'agent_metrics' or stream_name == 'schedule_metrics') and len(date_records) == 0:
             return
-                                                                      
+
         with metrics.record_counter(stream_name) as counter:
                 with Transformer() as transformer:
                     while parse is True:
-                        
-                        #parse through all date and user and channel records for Workstream Agent Metrics 
+
+                        #parse through all date and user and channel records for Workstream Agent Metrics
                         if(len(date_records)>=1):
-                            record_date = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records]['date'] 
-                            user_channel_records = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']]                            
-                            
-                            if(len(user_channel_records)>=1 and stream_name == 'agent_metrics'): 
-                                user_id = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['id']
-                                record_user_firstName = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['firstName']
-                                record_user_lastName = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['lastName']
-                                records = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records][endpoint['channel_key']]
+                            record_data = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records]
+                            record_date = record_data['date']
+                            user_channel_records = record_data[endpoint['user_key']]
+
+                            if(len(user_channel_records)>=1 and stream_name == 'agent_metrics'):
+                                user_record_data = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]
+                                user_id = user_record_data['id']
+                                record_user_firstName = user_record_data['firstName']
+                                record_user_lastName = user_record_data['lastName']
+                                records = user_record_data [endpoint['channel_key']]
                                 parsed_user_records += 1
-                            
-                            if(len(user_channel_records)>=1 and stream_name == 'schedule_metrics'): 
-                                user_id = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['id']
-                                record_user_email = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['email']
-                                record_user_rostered_startTime = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['rosteredStartTime']
-                                record_user_rostered_endTime = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['rosteredEndTime']
-                                record_user_acutal_startTime = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['actualStartTime']
-                                record_user_actual_endTime = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]['actualEndTime']
-                                records = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records][endpoint['channel_key']]
+
+                            if(len(user_channel_records)>=1 and stream_name == 'schedule_metrics'):
+                                user_record_data = data[endpoint['data_key']][endpoint['metric_key']][parsed_date_records][endpoint['user_key']][parsed_user_records]
+                                user_id = user_record_data['id']
+                                record_user_email = user_record_data['email']
+                                record_user_rostered_startTime = user_record_data['rosteredStartTime']
+                                record_user_rostered_endTime = user_record_data['rosteredEndTime']
+                                record_user_acutal_startTime = user_record_data['actualStartTime']
+                                record_user_actual_endTime = user_record_data['actualEndTime']
+                                records = user_record_data[endpoint['channel_key']]
                                 parsed_user_records += 1
-                            
+
                             if len(user_channel_records)==parsed_user_records:
-                                parsed_user_records = 0     
+                                parsed_user_records = 0
                                 parsed_date_records += 1
-                            
+
                         for record in records:
                             if persist and stream_name in selected_streams:
                                 record = {**record, **key_bag}
                                 try:
                                     record_typed = transformer.transform(record,
                                                                     schema,
-                                                                    mdata)  
-                                    
+                                                                    mdata)
+
                                     #Map 'date', 'id', 'firstName' and 'lastName' fields for Workstream Agent Metrics
                                     if (len(date_records)>=1 and stream_name== 'agent_metrics'):
                                         record_typed["id"] = user_id
                                         record_typed["date"] = record_date
                                         record_typed["firstName"] = record_user_firstName
                                         record_typed["lastName"] = record_user_lastName
-                                   
+
                                     #Map 'date', 'id', 'email', 'rosteredStartTime', 'rosteredEndTime', 'actualStartTime' and 'actualEndTime' fields for Schedule Metrics
                                     if (len(date_records)>=1 and stream_name== 'schedule_metrics'):
                                         record_typed["id"] = user_id
@@ -167,47 +166,36 @@ def sync_endpoint(client,
                                         record_typed["rosteredEndTime"] = record_user_rostered_endTime
                                         record_typed["actualStartTime"] = record_user_acutal_startTime
                                         record_typed["actualEndTime"] = record_user_actual_endTime
-                             
+
                                     # To ensure Email is all in lower_case for 'users' and 'tasks'
                                     if stream_name == 'users':
                                         record_typed['email'] = record_typed['email'].lower()
-                                    
+
                                     if stream_name == 'tasks':
                                         record_typed['userEmail'] = record_typed['userEmail'].lower()
-                                                                           
+
                                 except Exception as e:
                                     LOGGER.info("PLAYVOX Sync Exception: %s....Record: %s", e, record)
-                                
+
                                 singer.write_record(stream_name, record_typed)
-                                
                                 counter.increment()
-                        
-                        if len(date_records)==parsed_date_records:
+
+                        if len(date_records) == parsed_date_records:
                             parse = False
 
-        # Subtract 14 days from the current date and time for Schedule Metrics
-        startDate = datetime.utcnow() - timedelta(days=14)
-
-        # Set the time to midnight
-        startDate = startDate.replace(hour=0, minute=0, second=0, microsecond=0)
-                   
         # 1. set bookmark
-        if(stream_name == 'schedule_metrics'):
-            singer.write_bookmark(state, stream_name, 'endDate', str(startDate.isoformat()))
-        else:
-            singer.write_bookmark(state, stream_name, 'endDate', str(datetime.utcnow().isoformat()))
-            
+        singer.write_bookmark(state, stream_name, 'endDate', str(datetime.utcnow().isoformat()))
         # for records that expect to be paged
         if endpoint.get('paginate', True):
-            #get next_page_token
+            # get next_page_token
             next_page_token = data.get('nextPageToken', '')
-            #set 'nextPageToken' token when results have pages
+            # set 'nextPageToken' token when results have pages
             params = {
                         'nextPageToken': next_page_token
                     }
-                    
-def update_current_stream(state, stream_name=None):  
-    set_currently_syncing(state, stream_name) 
+
+def update_current_stream(state, stream_name=None):
+    set_currently_syncing(state, stream_name)
     singer.write_state(state)
 
 def get_required_streams(endpoints, selected_stream_names):
@@ -240,7 +228,7 @@ def sync(client, catalog, state):
     for stream_name, endpoint in ENDPOINTS_CONFIG.items():
         if stream_name in required_streams:
             update_current_stream(state, stream_name)
-            sync_endpoint(client,        
+            sync_endpoint(client,
                           catalog,
                           state,
                           required_streams,
@@ -250,5 +238,5 @@ def sync(client, catalog, state):
                           {})
 
     update_current_stream(state)
-    
+
     return state
